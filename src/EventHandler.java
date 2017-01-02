8c2cb0f9ce8fb503ddc4e4df36198fe332a13c56
TheMaxJ <maxjdebate@gmail.com>
2017-01-02 10:42:18 -0700

    Refactoring of MainWindow

---
+++ src/EventHandler.java
@@ -0,0 +1,77 @@
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+
+public class EventHandler {
+	
+	@SuppressWarnings("unchecked")
+	public static void listRightClicked(MouseEvent event) {
+		JList<String> list = (JList<String>) event.getComponent();
+		int row = list.locationToIndex(event.getPoint());
+        list.setSelectedIndex(row);  
+
+		
+	}
+	
+	public static void saveClicked() {
+		List<Player> players = new ArrayList<Player>();
+		for (int i = 0; i < MainWindow.getPlayerList().size(); i++) {
+			players.add(new Player(MainWindow.getPlayerList().getElementAt(i)));
+		}
+		DataManager.savePlayers(players);
+	}
+	
+	public static void newMatchClicked() {
+		if (MainWindow.getPlayerList().isEmpty()) return;
+		String name1 = "";
+		String name2 = "";
+		boolean cont = false;
+		do {
+			name1 = JOptionPane.showInputDialog("First player's name?");
+			if (name1 == null) return;
+			cont = MainWindow.contains(name1);
+			if (!cont) {
+				JOptionPane.showMessageDialog(null, "Could not find that player name.");
+			}
+		} while (!cont);
+		cont = false;
+		do {
+			if (MainWindow.getPlayerList().isEmpty()) return;
+			name2 = JOptionPane.showInputDialog("Second player's name?");
+			if (name2 == null) return;
+			cont = MainWindow.contains(name2);
+			if (!cont) {
+				JOptionPane.showMessageDialog(null, "Could not find that player name.");
+			}
+		} while (!cont);
+		
+		Player p1 = MainWindow.getPlayer(name1);
+		Player p2 = MainWindow.getPlayer(name2);
+		
+		String outcomes[] = {name1, name2, "Draw"};
+		
+		int i = JOptionPane.showOptionDialog(null, "Who won?", "Outcome", JOptionPane.DEFAULT_OPTION, 0, null, outcomes, outcomes[0]);
+		double score = .5;
+		if (i == 0) {
+			score = 1;
+		} else if (i == 1) {
+			score = 0;
+		}
+		p1.updateElo(p2, score);
+		p2.updateElo(p1, 1-score);
+		MainWindow.removePlayer(p1);
+		MainWindow.removePlayer(p2);
+		p1.pushElo();
+		p2.pushElo();
+		MainWindow.addPlayer(p1);
+		MainWindow.addPlayer(p2);
+	}
+	
+	public static void addPlayerClicked() {
+		String newPlayer = JOptionPane.showInputDialog("New player name");
+		MainWindow.getPlayerList().addElement(newPlayer + Main.eloSpacer + "1000");
+	}
+}
--- src/MainWindow.java
+++ src/MainWindow.java
@@ -3,16 +3,14 @@ import java.awt.EventQueue;
 import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
 import javax.swing.JList;
-import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
 import java.awt.BorderLayout;
 import javax.swing.JButton;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import javax.swing.JScrollPane;
 
 public class MainWindow {
 
@@ -50,7 +48,7 @@ public class MainWindow {
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		playerList = new DefaultListModel<String>();
-
+		JScrollPane scrollPane = new JScrollPane();
 		JList<String> list = new JList<String>(playerList);
 		ListPopup popup = new ListPopup();
 		popup.addMouseListener(new MouseAdapter() {
@@ -65,11 +63,13 @@ public class MainWindow {
 				
 		    }
 		});
-		//list.setComponentPopupMenu(new ListPopup());
 		list.addMouseListener(new MouseAdapter() {
-			public void mousePressed(MouseEvent e)  {check(e);}
-			public void mouseReleased(MouseEvent e) {check(e);}
-
+			public void mousePressed(MouseEvent e)  {
+				check(e);
+			}
+			public void mouseReleased(MouseEvent e) {
+				check(e);
+			}
 			public void check(MouseEvent e) {
 			    if (e.isPopupTrigger()) { //if the event shows the menu
 			        list.setSelectedIndex(list.locationToIndex(e.getPoint())); //select the item
@@ -78,10 +78,9 @@ public class MainWindow {
 			}
 			
 			@Override
-			public void mouseClicked(MouseEvent event) {
-				
+			public void mouseClicked(MouseEvent event) {	
 				if(SwingUtilities.isRightMouseButton(event)) {
-					rightClickList(event);
+					EventHandler.listRightClicked(event);
 				}
 			}
 		});
@@ -90,13 +89,14 @@ public class MainWindow {
 		for (Player player : loaded) {
 			addPlayer(player);
 		}
-		frame.getContentPane().add(list, BorderLayout.WEST);
+		frame.getContentPane().add(scrollPane, BorderLayout.WEST);
+		scrollPane.setViewportView(list);
 		
 		JButton btnAddPlayer = new JButton("Add Player");
 		btnAddPlayer.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
-				addPlayerPressed();
+				EventHandler.addPlayerClicked();
 			}
 		});
 		frame.getContentPane().add(btnAddPlayer, BorderLayout.EAST);
@@ -105,7 +105,7 @@ public class MainWindow {
 		btnNewMatch.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				newMatchPressed();
+				EventHandler.newMatchClicked();
 			}
 		});
 		frame.getContentPane().add(btnNewMatch, BorderLayout.SOUTH);
@@ -114,10 +114,10 @@ public class MainWindow {
 		btnSave.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				savePressed();
+				EventHandler.saveClicked();
 			}
 		});
-		frame.getContentPane().add(btnSave, BorderLayout.NORTH);	
+		frame.getContentPane().add(btnSave, BorderLayout.NORTH);
 	}
 	
 	public static boolean contains(String query) {
@@ -142,88 +142,6 @@ public class MainWindow {
 	
 	public static void removePlayer(Player player) {
 		playerList.removeElement(player.toString());
-	}
-	
-	public void dupdateLists() {
-		List<String> alphabetical = new ArrayList<String>();
-		for (int i = 0; i < playerList.size(); i++) {
-			alphabetical.add(playerList.getElementAt(i));
-		}
-		
-		Collections.sort(alphabetical, String.CASE_INSENSITIVE_ORDER);
-		
-		playerList.clear();
-		
-		for (String s : alphabetical) {
-			playerList.addElement(s);
-		}
-
-	}
-	
-	public void addPlayerPressed() {
-		String newPlayer = JOptionPane.showInputDialog("New player name");
-		playerList.addElement(newPlayer + Main.eloSpacer + "1000");
-	}
-	
-	public void newMatchPressed() {
-		if (playerList.isEmpty()) return;
-		String name1 = "";
-		String name2 = "";
-		boolean cont = false;
-		do {
-			name1 = JOptionPane.showInputDialog("First player's name?");
-			cont = contains(name1);
-			if (!cont) {
-				JOptionPane.showMessageDialog(null, "Could not find that player name.");
-			}
-		} while (!cont);
-		cont = false;
-		do {
-			if (playerList.isEmpty()) return;
-			name2 = JOptionPane.showInputDialog("Second player's name?");
-			cont = contains(name2);
-			if (!cont) {
-				JOptionPane.showMessageDialog(null, "Could not find that player name.");
-			}
-		} while (!cont);
-		
-		Player p1 = getPlayer(name1);
-		Player p2 = getPlayer(name2);
-		
-		String outcomes[] = {name1, name2, "Draw"};
-		
-		int i = JOptionPane.showOptionDialog(null, "Who won?", "Outcome", JOptionPane.DEFAULT_OPTION, 0, null, outcomes, outcomes[0]);
-		double score = .5;
-		if (i == 0) {
-			score = 1;
-		} else if (i == 1) {
-			score = 0;
-		}
-		p1.updateElo(p2, score);
-		p2.updateElo(p1, 1-score);
-		removePlayer(p1);
-		removePlayer(p2);
-		p1.pushElo();
-		p2.pushElo();
-		addPlayer(p1);
-		addPlayer(p2);
-	}
-	
-	public void savePressed() {
-		List<Player> players = new ArrayList<Player>();
-		for (int i = 0; i < playerList.size(); i++) {
-			players.add(new Player(playerList.getElementAt(i)));
-		}
-		DataManager.savePlayers(players);
-	}
-	
-	@SuppressWarnings("unchecked")
-	public void rightClickList(MouseEvent event) {
-		JList<String> list = (JList<String>) event.getComponent();
-		int row = list.locationToIndex(event.getPoint());
-        list.setSelectedIndex(row);  
-
-		
 	}
 	
 	public static DefaultListModel<String> getPlayerList() {
